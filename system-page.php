79a3aac5e333167fcb2a2d018da4d6cdc38c5382
tertek <ekin.tertemiz@swisstph.ch>
2025-02-03 12:04:23 +0100

    add control center page

---
+++ system-page.php
@@ -0,0 +1,32 @@
+<?php  namespace STPH\massSendIt;
+$pids = $module->getProjectsWithModuleEnabled();
+$projects = [];
+
+//  Gather project data
+foreach ($pids as $key => $pid) {
+    $p = $module->getProject($pid);
+    $bulkModel = new BulkModel($module, $pid);
+
+    $project = (object) array(
+        "title" => $p->getTitle(),
+        "id" => $pid,
+        "bulks" => $bulkModel->getAll()
+    );
+    $projects[] = $project;
+}
+?>
+<h2>Mass Send-It Admin</h2>
+<p>This page is for administrative purposes of Mass Send-It module.</p>
+
+
+<b>Module usage</b>
+<p>Module is being used in <?=count($projects)?> projects.</p>
+<ul>
+<?php foreach($projects as $key=>$project): ?>
+    <li><?= $project->title ?> (<?= $project->id ?>), Bulks: <?= count($project->bulks) ?></li>
+    
+<?php endforeach ?>
+</ul>
+
+
+
